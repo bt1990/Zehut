d8f091dc77b4f52acfb803f4803c436c1d1ab68a
Charles Florestal <florestal.cw@gmail.com>
2021-11-24 08:25:06 -0500

    Established Test File

---
+++ Test.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "TSensor.h"
+#include "TBoard.h"
+
+
+
+/***** BOARD'S CLASS AND METHODS TESTS*****/
+
+
+
+/***** SENSOR'S CLASS AND METHODS TESTS*****/
+
+int main(){
+
+	printf("Got it!!!");
+	return 0;	
+}
